26adb66d53fc7b7e5ae8c08f2db5601d3c1bbc38
rachelori <rachelori@gmail.com>
2017-01-05 18:35:19 -0800

    Modifications to match to Codecademy solution

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
   var $menu = $("#sidebar-wrapper");
-  $(document).on("click", ".js-menu-open", function(evt) {
+
+  $(document)
+    .on("click", ".js-menu-open", function(evt) {
     $menu.addClass("open");
     return evt.target.tagName === "A";
-  });
+  })
 
-  $(document).on("click", ".js-menu-close", function(evt) {
+   .on("click", ".js-menu-close", function(evt) {
     $menu.removeClass("open");
     return evt.target.tagName === "A";
   });
